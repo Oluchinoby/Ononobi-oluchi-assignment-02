3d6f6108e3d93b9da93e548d3b17d818fdb92718
Oluchi Joy Ononobi <108630959+Oluchinoby@users.noreply.github.com>
2022-07-04 21:46:35 +0000

    Attempt challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -16,12 +16,12 @@ const assignment = {};
 function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
 
 let initialValue = 0
-let sum = [{x: 1}, {x: 2}, {x: 3}].reduce(function (previousValue, currentValue) {
-    return previousValue + currentValue.x
-}, initialValue)
+let sum = [{x: 1}, {x: 2}, {x: 3}].reduce(
+    (previousValue, currentValue) => previousValue + currentValue.x
+    , initialValue
+)
 
 console.log(sum) // logs 6
 
