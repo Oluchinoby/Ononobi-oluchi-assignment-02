070f7093553aa59b8b201565b76455b07f0dfff3
Oluchi Joy Ononobi <108630959+Oluchinoby@users.noreply.github.com>
2022-07-09 12:28:39 +0000

    check Challenge 1

--- src/assignment.js
+++ src/assignment.js
@@ -17,7 +17,7 @@ function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
 
-const array = [1, 2, 3, 4];
+const array = [1, 2, 3, 4, 5];
 let sum = 0;
 
 for (let i = 0; i < array.length; i++) {
